a2fc06b100028c52f5be760c39d6e827eef3af9d
MinhQuan Tran <minhquan.tran@wsu.edu>
2019-12-05 22:54:11 -0800

    Made nodemodules default, add comments on script

--- run.py
+++ run.py
@@ -11,11 +11,16 @@ import signal
 print("This script will open the PlanHub website")
 x = input("Do you want to open the website? [Y/y for yes]: ")
 if x.lower() == 'y':
+    # Get current directory
     original_dir = os.getcwd()
+    # Get ./Planhub_App
     react_start = str(original_dir) + "/Planhub_App"
+    # Get ./Planhub_App/src/backend
     mongo_start = react_start + "/src/backend"
+    # Run mongodb
     os.chdir(mongo_start)
     sp.Popen(["nodemon", "server.js"])
+    # Run React App
     os.chdir(react_start)
     react = sp.run(["npm", "start"]) 
     exit()
