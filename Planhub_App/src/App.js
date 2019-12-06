335e1f0b37bad8ac54ed21aa7b81adf3f54bcc3e
jacksonoah <noah.jackson@wsu.edu>
2019-12-06 01:14:10 -0800

    fixing names

--- Planhub_App/src/App.js
+++ Planhub_App/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import { MainApp } from './components/appComponent'
+import { MainApp } from './components/schedule'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { HomePage } from './components/homePage'
 
 function App() {
   return (
     <Router>
-      <Route path="/appComponent" component={MainApp} />
+      <Route exact path="/schedule" component={MainApp} />
       <Route path="/" component={HomePage} />
     </Router>
   );
